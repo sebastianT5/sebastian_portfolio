69e0f729debe727ff17564960028e78803a0055a
sebastianT5 <125829798+sebastianT5@users.noreply.github.com>
2024-07-26 09:55:15 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Scroll section active link
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150; // Fix the assignment operator here
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      navlinks.forEach(link => { // Change 'links' to 'link' here
+        link.classList.remove('active');
+      });
+      document.querySelector('header nav a[href*=' + id + ']').classList.add('active'); // Fix the typo 'hred' to 'href'
+    }
+  });
+};
